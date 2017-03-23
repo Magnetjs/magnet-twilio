ec5a4903e6e9b1c7d65f42a951abc33f195b8ed5
Kieve Chua <kievechua@gmail.com>
2017-03-23 11:58:11 +0800

    use fly and migrate away from convert

---
+++ src/config/twilio.ts
@@ -0,0 +1,5 @@
+// https://twilio.github.io/twilio-node/2.1.0/index.html#quickstart
+export default {
+  accountSid: '', // Your Account SID from www.twilio.com/console
+  authToken: '' // Your Auth Token from www.twilio.com/console
+}
